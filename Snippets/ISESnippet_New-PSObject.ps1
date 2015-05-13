e35d6b3fa6419f2869d4e8afcf7b18c98850e05f
joshatwell <jatwell@jatwell-lt.local>
2015-05-13 08:13:58 -0400

    MinDotNetVersion Function and PSObject snippet
    Adding a function for checking .NET version on a windows system and a
    ISE Snippet for adding a new PSObject in a script.

---
+++ Snippets/ISESnippet_New-PSObject.ps1
@@ -0,0 +1,15 @@
+﻿[scriptblock]$code = 
+{
+$object = New-Object -TypeName PSObject -Property @{
+      Name = $Server
+      ServerUri = "http://$server/"
+      Authorization = $authorization
+}
+}
+ 
+New-IseSnippet `
+    -Text $code.ToString() `
+    -CaretOffset $code.ToString().Length `
+    -Force `
+    -Title ‘NewObject’ `
+    -Description ‘Use for creating a new PowerShell Object’
